59aa405be978c46a6b6eb465fac9635198126eb6
Dat Tran <dat.tran@ghielectronics.com>
2021-05-26 12:53:53 -0400

    fixed i2c4

--- ports/stm32/boards/SC13048Q/mpconfigboard.h
+++ ports/stm32/boards/SC13048Q/mpconfigboard.h
@@ -7,7 +7,7 @@
 #define MICROPY_HW_ENABLE_RTC       (0)
 #define MICROPY_HW_ENABLE_RNG       (1)
 #define MICROPY_HW_ENABLE_DAC       (1)
-#define MICROPY_HW_ENABLE_SERVO     (0) // SERVO requires TIM5 (not on L452).
+#define MICROPY_HW_ENABLE_SERVO     (0)
 #define MICROPY_HW_HAS_SWITCH       (0)
 
 
@@ -31,33 +31,29 @@
 #define MICROPY_HW_UART3_RX     (pin_B11)
 #define MICROPY_HW_UART4_TX     (pin_A0)
 #define MICROPY_HW_UART4_RX     (pin_A1)
-// USART1 is connected to the ST-LINK USB VCP
+// USART2 is connected to the ST-LINK USB VCP
 #define MICROPY_HW_UART_REPL        PYB_UART_2
 #define MICROPY_HW_UART_REPL_BAUD   115200
 
 // I2C busses
-#define MICROPY_HW_I2C1_SCL     (pin_B8)    // Arduino D15, pin 3 on CN10
-#define MICROPY_HW_I2C1_SDA     (pin_B9)    // Arduino D14, pin 5 on CN10
-#define MICROPY_HW_I2C2_SCL     (pin_B13)   // Arduino D6,  pin 25 on CN10
-#define MICROPY_HW_I2C2_SDA     (pin_B14)   //              pin 18 on CN10
-#define MICROPY_HW_I2C3_SCL     (pin_A7)    //              pin 15 on CN10
-#define MICROPY_HW_I2C3_SDA     (pin_B4)    //              pin 27 on CN10
-#define MICROPY_HW_I2C4_SCL     (pin_C0)    //              pin 38 on CN7
-#define MICROPY_HW_I2C4_SDA     (pin_C1)    //              pin 36 on CN7
+#define MICROPY_HW_I2C1_SCL     (pin_B8)
+#define MICROPY_HW_I2C1_SDA     (pin_B9)
+#define MICROPY_HW_I2C2_SCL     (pin_B13)
+#define MICROPY_HW_I2C2_SDA     (pin_B14)
+#define MICROPY_HW_I2C3_SCL     (0)
+#define MICROPY_HW_I2C3_SDA     (0)
+#define MICROPY_HW_I2C4_SCL     (pin_B10)
+#define MICROPY_HW_I2C4_SDA     (pin_B11)
 
 // SPI busses
-#define MICROPY_HW_SPI1_NSS     (pin_A15)   //              pin 17 on CN7
-#define MICROPY_HW_SPI1_SCK     (pin_B3)    // Arduino D13, pin 11 on CN10
-#define MICROPY_HW_SPI1_MISO    (pin_B4)    // Arduino D12, pin 13 on CN10
-#define MICROPY_HW_SPI1_MOSI    (pin_B5)    // Arduino D11, pin 15 on CN10
-#define MICROPY_HW_SPI2_NSS     (pin_B12)   //              pin 16 on CN10
-#define MICROPY_HW_SPI2_SCK     (pin_B13)   //              pin 30 on CN10
-#define MICROPY_HW_SPI2_MISO    (pin_B14)   //              pin 28 on CN10
-#define MICROPY_HW_SPI2_MOSI    (pin_B15)   //              pin 26 on CN10
-#define MICROPY_HW_SPI3_NSS     (pin_A4)    //              pin 32 on CN7
-#define MICROPY_HW_SPI3_SCK     (pin_C10)   //              pin 1 on CN7
-#define MICROPY_HW_SPI3_MISO    (pin_C11)   //              pin 2 on CN7
-#define MICROPY_HW_SPI3_MOSI    (pin_C12)   //              pin 3 on CN7
+#define MICROPY_HW_SPI1_NSS     (pin_A15)
+#define MICROPY_HW_SPI1_SCK     (pin_B3)
+#define MICROPY_HW_SPI1_MISO    (pin_B4)
+#define MICROPY_HW_SPI1_MOSI    (pin_B5)
+#define MICROPY_HW_SPI2_NSS     (pin_B12)
+#define MICROPY_HW_SPI2_SCK     (pin_B13)
+#define MICROPY_HW_SPI2_MISO    (pin_B14)
+#define MICROPY_HW_SPI2_MOSI    (pin_B15)
 
 // CAN busses
 #define MICROPY_HW_CAN1_TX (pin_B6)
