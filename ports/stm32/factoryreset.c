a1e0016c19932813101a365a689329d289208d72
Dat Tran <dat.tran@ghielectronics.com>
2021-08-17 11:35:29 -0400

    add banner

--- ports/stm32/factoryreset.c
+++ ports/stm32/factoryreset.c
@@ -61,7 +61,15 @@ static const char fresh_boot_py[] =
 
 static const char fresh_main_py[] =
 	"from BrainPad import *\r\n"
-    "# main.py -- put your code here!\r\n"
+	"if (IsPulse == True):\r\n"
+	"	Print('GHI Electronics, LLC')\r\n"
+	"	Print('   BrainPad Pulse')\r\n"
+	"	Print('    MicroPython')\r\n"
+	"	Print(' ')\r\n"
+	"	Print(' ')\r\n"
+	"else:\r\n"
+	"	Print('GHI - MicroPython')\r\n"
+    	"# main.py -- put your code here!\r\n"
 ;
 
 #if MICROPY_HW_ENABLE_USB
