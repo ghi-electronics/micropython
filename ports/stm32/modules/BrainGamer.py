e4db3958ef71c129f7c3c269cfd7c7dfef594cc0
Dat Tran <dat.tran@ghielectronics.com>
2021-08-24 15:27:51 -0400

    add Gamer

---
+++ ports/stm32/modules/BrainGamer.py
@@ -0,0 +1,88 @@
+from BrainPad import *
+
+class GamerButton:
+    buttonScanTime = 0.25;    
+    buttonUp = Button(P14, buttonScanTime);
+    buttonDown = Button(P15, buttonScanTime);
+    buttonLeft = Button(P13, buttonScanTime);
+    buttonRight = Button(P16, buttonScanTime);
+
+def ButtonScanTime(value):
+    GamerButton.buttonScanTime = value    
+    GamerButton.buttonUp = Button(P14, value);
+    GamerButton.buttonDown = Button(P15, value);
+    GamerButton.buttonLeft = Button(P13, value);
+    GamerButton.buttonRight = Button(P16, value);
+    
+rockerX = Analog(P4);
+rockerY = Analog(P3);
+motor = Digital(P8);
+sound = Sound(Buzzer, 0.2, 50);
+    
+def Beep():
+    Out(sound, 1000)
+    
+def ButtonUp():
+    return In(GamerButton.buttonUp) != 0;
+
+def ButtonDown():
+    return In(GamerButton.buttonDown) != 0;
+
+def ButtonLeft():
+    return In(GamerButton.buttonLeft) != 0;
+
+def ButtonRight():
+    return In(GamerButton.buttonRight) != 0;
+    
+def RockerX():
+    return In(rockerX)
+
+def RockerY():
+    return In(rockerY)
+
+def VibrateOn():
+    Out(motor, 0);
+    
+def VibrateOff():
+    Out(motor, 1);
+    
+    
+    
+        
+        
+            
+            
+        
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
