fc4912f4cd3e06f129d6fa1a5f74c333496a1652
Dat Tran <dat.tran@ghielectronics.com>
2021-09-10 15:28:01 -0400

    fix tick on gamer

--- ports/stm32/modules/BrainGamer.py
+++ ports/stm32/modules/BrainGamer.py
@@ -5,7 +5,10 @@ class GamerButton:
     buttonUp = Button(P14, buttonScanTime);
     buttonDown = Button(P15, buttonScanTime);
     buttonLeft = Button(P13, buttonScanTime);
-    buttonRight = Button(P16, buttonScanTime);
+    if IsPulse==True:
+        buttonRight = Button(P16, buttonScanTime);
+    else:
+        buttonRight = Digital(P16);
 
 def ButtonScanTime(value):
     GamerButton.buttonScanTime = value    
@@ -14,21 +17,25 @@ def ButtonScanTime(value):
     GamerButton.buttonLeft = Button(P13, value);
     GamerButton.buttonRight = Button(P16, value);
     
-rockerX = Analog(P4);
-rockerY = Analog(P3);
-motor = Digital(P8);
-sound = Sound(P0, 0, 50);
+if IsPulse==True:
+    rockerX = Analog(P4);
+    rockerY = Analog(P3);
+    motor = Digital(P8);
+    sound = Sound(P0, 0, 50);
     
 def Beep():
-    Out(sound, 1000)
-    Wait(0.2)
-    Out(sound, 0)
+    if IsPulse==True:
+        Out(sound, 1000)
+        Wait(0.2)
+        Out(sound, 0)
     
 def SoundOn(frequency = 1000):
-    Out(sound, frequency)
+    if IsPulse==True:
+        Out(sound, frequency)
     
 def SoundOff():
-    Out(sound, 0)
+    if IsPulse==True:
+        Out(sound, 0)
     
 def ButtonUp():
     return In(GamerButton.buttonUp) != 0;
@@ -40,19 +47,30 @@ def ButtonLeft():
     return In(GamerButton.buttonLeft) != 0;
 
 def ButtonRight():
-    return In(GamerButton.buttonRight) != 0;
+    if IsPulse==True:
+        return In(GamerButton.buttonRight) != 0;
+    else:
+        return In(GamerButton.buttonRight) == 0;
     
 def RockerX():
-    return In(rockerX)
+    if IsPulse==True:
+        return In(rockerX)
+    else:
+        return 0
 
 def RockerY():
-    return In(rockerY)
+    if IsPulse==True:
+        return In(rockerY)
+    else:
+        return 0
 
 def VibrateOn():
-    Out(motor, 0);
+    if IsPulse==True:
+        Out(motor, 0);
     
 def VibrateOff():
-    Out(motor, 1);
+    if IsPulse==True:
+        Out(motor, 1);
     
     
     
